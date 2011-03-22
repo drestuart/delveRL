b3f95f2d2270b047f31ca456e1cc43d51c10df28
drestuart <drestuart@6a361d6c-2ae1-ea7f-298d-c6c5c9226c6f>
2011-03-22 22:35:06 +0000

    Made a main function.  Had to make lotsa globals, sadly.

--- trunk/src/delveRL.py
+++ trunk/src/delveRL.py
@@ -1,13 +1,12 @@
 import libtcodpy as libtcod
-import textwrap
 import math
+import textwrap
 
 ############################################################
 #
 # Constants
 #
 ############################################################
-
 
 # Max monsters per room
 MAX_ROOM_MONSTERS = 3
@@ -933,88 +932,97 @@ def closest_monster(max_range):
 
 
 
+def main():
 
-
-############################################################
-#
-# Set up stuff and enter main loop
-#
-############################################################
-
-#create the list of game messages and their colors, starts empty
-
-game_msgs = []
-
-# Select font -- arial 10pt
-libtcod.console_set_custom_font('fonts/arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
-
-# Open window.  The parameters are: width, height (in characters?),
-# title, and whether to full-screen or not.
-libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'leRogueLike 0.0.0', False)
-
-# The main console!
-con = libtcod.console_new(MAP_WIDTH, MAP_HEIGHT)
-
-# The GUI panel
-panel = libtcod.console_new(SCREEN_WIDTH, PANEL_HEIGHT)
-
-
-# Limit framerate, if you *really* have to make a realtime RL
-libtcod.sys_set_fps(LIMIT_FPS)
-
-#create object representing the player
-fighter_component = Fighter(hp=30, defense=2, power=5,
-                            death_function=player_death)
-player = Object(0, 0, '@', 'player', libtcod.white, blocks=True, fighter=fighter_component)
- 
-#the list of objects starting with the player
-objects = [player]
-
-# The inventory
-inventory = []
-
-
-
-# Make the map!
-make_map()
-
-# Give libtcod the map?
-fov_map = libtcod.map_new(MAP_WIDTH, MAP_HEIGHT)
-for y in range(MAP_HEIGHT):
-    for x in range(MAP_WIDTH):
-        libtcod.map_set_properties(fov_map, x, y, 
-                                   not map[x][y].blocked, 
-                                   not map[x][y].block_sight)
-
-
-# Initialize the player status variables
-game_state = 'playing'
-player_action = None
-
-#a warm welcoming message!
-message('Welcome stranger! Prepare to perish in the Tombs of the Ancient Kings.', libtcod.red)
-
-# Run the game while the window is open
-while not libtcod.console_is_window_closed():
-    # Genius goes here
-        
-    render_all()
- 
     
- 
-    #erase all objects at their old locations, before they move
-    for object in objects:
-        object.clear()
- 
-    #handle keys and exit game if needed
-    player_action = handle_keys()
-    if player_action == 'exit':
-        break
+    ############################################################
+    #
+    # Set up stuff and enter main loop
+    #
+    ############################################################
+    
+    # Globals.  Sorry everyone.
+    global objects, con, panel, player, inventory, game_state, player_action, game_msgs, fov_map
+    
+    
+    #create the list of game messages and their colors, starts empty
+    
+    game_msgs = []
+    
+    # Select font -- arial 10pt
+    libtcod.console_set_custom_font('../fonts/arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+    
+    # Open window.  The parameters are: width, height (in characters?),
+    # title, and whether to full-screen or not.
+    libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'leRogueLike 0.0.0', False)
+    
+    # The main console!
+    con = libtcod.console_new(MAP_WIDTH, MAP_HEIGHT)
+    
+    # The GUI panel
+    panel = libtcod.console_new(SCREEN_WIDTH, PANEL_HEIGHT)
+    
+    
+    # Limit framerate, if you *really* have to make a realtime RL
+    libtcod.sys_set_fps(LIMIT_FPS)
+    
+    #create object representing the player
+    fighter_component = Fighter(hp=30, defense=2, power=5,
+                                death_function=player_death)
+    player = Object(0, 0, '@', 'player', libtcod.white, blocks=True, fighter=fighter_component)
+     
+    #the list of objects starting with the player
+    objects = [player]
 
-    #let monsters take their turn
-    if game_state == 'playing' and player_action != 'didnt-take-turn':
+    
+    # The inventory
+    inventory = []
+    
+    
+    
+    # Make the map!
+    make_map()
+    
+    # Give libtcod the map?
+    fov_map = libtcod.map_new(MAP_WIDTH, MAP_HEIGHT)
+    for y in range(MAP_HEIGHT):
+        for x in range(MAP_WIDTH):
+            libtcod.map_set_properties(fov_map, x, y, 
+                                       not map[x][y].blocked, 
+                                       not map[x][y].block_sight)
+    
+    
+    # Initialize the player status variables
+    game_state = 'playing'
+    player_action = None
+    
+    #a warm welcoming message!
+    message('Welcome stranger! Prepare to perish in the Tombs of the Ancient Kings.', libtcod.red)
+    
+    # Run the game while the window is open
+    while not libtcod.console_is_window_closed():
+        # Genius goes here
+            
+        render_all()
+     
+        
+     
+        #erase all objects at their old locations, before they move
         for object in objects:
-            if object.ai:
-                object.ai.take_turn()
+            object.clear()
+     
+        #handle keys and exit game if needed
+        player_action = handle_keys()
+        if player_action == 'exit':
+            break
+    
+        #let monsters take their turn
+        if game_state == 'playing' and player_action != 'didnt-take-turn':
+            for object in objects:
+                if object.ai:
+                    object.ai.take_turn()
 
 
+
+if __name__ == '__main__':
+    main() 
