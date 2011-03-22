61f2a8e566693d079c0b22ab144ba63e74718d5e
drestuart <drestuart@6a361d6c-2ae1-ea7f-298d-c6c5c9226c6f>
2011-03-22 23:48:14 +0000

    Some Tile and Feature work

--- src/TileClass.py
+++ src/TileClass.py
@@ -1,14 +1,48 @@
 # External imports
 import libtcodpy as libtcod
 
+# Internal imports
+from DungeonFeatureClass import *
+
+# Constants
+
+# Wall and ground colors
+color_dark_wall = libtcod.Color(0, 0, 100)
+color_light_wall = libtcod.Color(130, 110, 50)
+color_dark_ground = libtcod.Color(50, 50, 150)
+color_light_ground = libtcod.Color(200, 180, 50)
+
 # The Tile class
 
 class Tile:
     #a tile of the map and its properties
-    def __init__(self, blocked, block_sight = None):
-        self.blocked = blocked
-        self.explored = False
- 
-        #by default, if a tile is blocked, it also blocks sight
-        if block_sight is None: block_sight = blocked
+    def __init__(self, block_move = False, block_sight = False, symbol = '.', 
+                 color = color_light_ground, background = libtcod.BKGND_NONE, 
+                 feature = None):
+        
+        self.block_move = block_move
         self.block_sight = block_sight
+        self.symbol = symbol
+        self.color = color
+        self.background = background
+        
+        if feature:
+            self.feature = feature
+            
+    def toDraw(self): 
+        return self.symbol, self.color, self.background
+    
+    def blocks_move(self):
+        return self.block_move
+    
+    def blocks_sight(self):
+        return self.block_sight
+                
+            
+def main():
+    tile = Tile(symbol = 'x')  
+    print tile.color
+            
+if __name__ == '__main__':
+    main()
+
