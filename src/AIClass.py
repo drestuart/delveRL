1b53ee9aa0d68d83b8e14a0a921c95b4975f66d3
drestuart <drestuart@6a361d6c-2ae1-ea7f-298d-c6c5c9226c6f>
2011-05-13 22:13:15 +0000

    Delegate movement to AIClass

--- src/AIClass.py
+++ src/AIClass.py
@@ -1,8 +1,12 @@
+# External imports
+import libtcodpy as libtcod
 
 CONFUSE_NUM_TURNS = 10
 
 class AI:
-    pass
+    
+    def setOwner(self, creature):
+        self.owner = creature
 
 class ConfusedMonster(AI):
     #AI for a temporarily confused monster (reverts to previous AI
@@ -30,9 +34,15 @@ class ConfusedMonster(AI):
 
 
 # Normal Monster class
-class NormalMonster(AI):
+class NormalMonsterAI(AI):
     #AI for a basic monster.
-    def take_turn(self):
+    def takeTurn(self):
+        dx = libtcod.random_get_int(0, -1, 1)
+        dy = libtcod.random_get_int(0, -1, 1)
+        self.owner.move(dx, dy)
+        print self.owner.name, "takes its turn"
+    
+    def _take_turn(self):
         #a basic monster takes its turn. If you can see it, it can see you
         monster = self.owner
         if libtcod.map_is_in_fov(fov_map, monster.x, monster.y):
@@ -44,3 +54,38 @@ class NormalMonster(AI):
             #close enough, attack! (if the player is still alive.)
             elif player.fighter.hp > 0:
                 monster.fighter.attack(player)
+                
+class PlayerAI(AI):
+    
+    def takeTurn(self):
+        print "Waiting for player"
+        #key = libtcod.console_check_for_keypress()  #real-time
+        key = libtcod.console_wait_for_keypress(True)  #turn-based
+        
+        if key.vk == libtcod.KEY_ESCAPE:
+            exit(0)  #exit game
+ 
+        elif key.vk == libtcod.KEY_UP or key.vk == libtcod.KEY_KP8:
+            self.owner.move(0, -1)
+ 
+        elif key.vk == libtcod.KEY_DOWN or key.vk == libtcod.KEY_KP2:
+            self.owner.move(0, 1)
+ 
+        elif key.vk == libtcod.KEY_LEFT or key.vk == libtcod.KEY_KP4:
+             self.owner.move(-1, 0)
+ 
+        elif key.vk == libtcod.KEY_RIGHT or key.vk == libtcod.KEY_KP6:
+            self.owner.move(1, 0)
+
+        elif key.vk == libtcod.KEY_KP1:
+            self.owner.move(-1, 1)
+
+        elif key.vk == libtcod.KEY_KP3:
+            self.owner.move(1, 1)
+            
+        elif key.vk == libtcod.KEY_KP7:
+            self.owner.move(-1, -1)
+        
+        elif key.vk == libtcod.KEY_KP9:
+            self.owner.move(1, -1)
+
